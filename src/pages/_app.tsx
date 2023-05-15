3ebaa77a79f963ef4b3a909137d16977a16490bb
Bailey Pumfleet <bailey@pumfleet.co.uk>
2023-05-15 17:16:07 +0100

    Add auto animate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Inter, Outfit } from "next/font/google";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 import { api } from "~/utils/api";
 
@@ -14,14 +15,17 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [parent] = useAutoAnimate();
   return (
     <SessionProvider session={session}>
       <style jsx global>{`
-        h1, h2, h3 {
+        h1,
+        h2,
+        h3 {
           font-family: ${outfit.style.fontFamily};
         }
       `}</style>
-      <div className={inter.className}>
+      <div className={inter.className} ref={parent}>
         <Component {...pageProps} />
       </div>
     </SessionProvider>
